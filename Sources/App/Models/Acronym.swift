c450ab63213f9cb59d8c638e75973bd401769b83
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-04-25 09:33:43 -0400

    Created Acronym class that conforms to the model protocol

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -5,4 +5,37 @@
 //  Created by Vinojen Gengatharan on 2021-04-24.
 //
 
-import Foundation
+import Vapor
+import Fluent
+
+// 1
+final class Acronym : Model{
+    
+    //2
+    static let schema = "acronyms"
+    
+    //3
+    @ID
+    var id: UUID?
+    
+    //4
+    @Field(key : "short")
+    var short : String
+    
+    @Field(key: "long")
+    var long : String
+    
+    // 5
+    init(){}
+    
+    //6
+    init(id : UUID? = nil, short : String, long : String) {
+        self.id = id
+        self.short = short
+        self.long = long
+    }
+    
+    
+    
+    
+}
